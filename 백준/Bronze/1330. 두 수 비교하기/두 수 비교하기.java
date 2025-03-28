f4a9deec00e9641b3dac3fb024333fbf4926dada
한동균 <107734276+hdg5639@users.noreply.github.com>
2025-03-29 00:02:01 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        String[] words = line.split(" ");
+        System.out.println(Integer.parseInt(words[0])>Double.parseDouble(words[1])?
+                ">":Integer.parseInt(words[0])<Double.parseDouble(words[1])?
+                "<":"==");
+    }
+}

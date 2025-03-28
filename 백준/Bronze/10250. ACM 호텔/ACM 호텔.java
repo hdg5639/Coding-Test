8f5a7397d386fdd6b86d0990cc9f4b4639e21b89
한동균 <107734276+hdg5639@users.noreply.github.com>
2025-03-29 06:20:58 +0900

    [Bronze III] Title: ACM 호텔, Time: 112 ms, Memory: 14364 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[][] s =  new String[n][3];
+        for (int i = 0; i < n; i++) {
+            s[i] = br.readLine().split(" ");
+        }
+        for (String[] row : s) {
+            int temp = Integer.parseInt(row[2]) % Integer.parseInt(row[0]);
+            int result = (temp == 0?Integer.parseInt(row[0]):temp) * 100 +
+                    (int)Math.ceil(Double.parseDouble(row[2]) / Double.parseDouble(row[0]));
+            System.out.println(result);
+        }
+    }
+}

2940088b450d510c941056d597603a9dc2edc544
한동균 <107734276+hdg5639@users.noreply.github.com>
2025-03-29 01:30:48 +0900

    [Bronze V] Title: 새싹, Time: 92 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.print("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |\n");
+    }
+}
